8cf57db7ac0f3d316feaa3ddfd4afa482556f54b
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-05-30 07:58:40 +0100

    basic pagination with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Data from "./Data.json"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1)
+  const recordsPerPage= 3;
+  const lastIndex = currentPage * recordsPerPage;
+  const firstIndex = lastIndex - recordsPerPage;
+  const records = Data.slice(firstIndex, lastIndex)
+  const npage = Math.ceil(Data.length /recordsPerPage)
+  const numbers = [...Array(npage + 1).keys()].slice(1)
+
+
+  function nextPage () {
+    if(currentPage !== npage){
+      setCurrentPage(currentPage + 1)
+    }
+  }
+
+  function prePage () {
+    if (currentPage !== 1){
+      setCurrentPage(currentPage -1)
+    }
+  }
+
+  function changeCurrentPage (id){
+    setCurrentPage(id)
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <table className='table '>
+    <thread>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Email</th>
+    </thread>
+    <tbody>
+      {records.map((d,i) => (
+        <tr key={i}>
+          <td>{d.ID}</td>
+          <td>{d.Name}</td>
+          <td>{d.email}</td>
+        </tr>
+      ))}
+    </tbody>
+   </table>
+   <nav>
+    <ul className='pagination'>
+      <li className="page-item">
+        <a href='#' className='page-link' onClick={prePage}>Prev</a>
+      </li>
+      {numbers.map((n, i) => (
+        <li className={`page-item ${currentPage === n ? 'active':''}`} key={i}>
+          <a href="#" className='page-link' onClick={() =>changeCurrentPage(n)}>{n}</a>
+        </li>
+      ))
+}
+<li className='page-item'>
+  <a href='#' className='page-link' onClick={nextPage}>Next</a>
+</li>
+
+      
+    </ul>
+   </nav>
+      
     </div>
   );
 }
